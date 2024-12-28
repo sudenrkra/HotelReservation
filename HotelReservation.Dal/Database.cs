d19e3c454fe1a68085d6a6d69ec1f3cb2c76b890
Sude Nur Kara <sudenrkra@gmail.com>
2024-12-29 00:25:55 +0300

    init

--- HotelReservation.Dal/Database.cs
+++ HotelReservation.Dal/Database.cs
@@ -1,7 +1,13 @@
-﻿namespace HotelReservation.Dal
+﻿
+using MySql.Data.MySqlClient;
+namespace HotelReservation.Dal
 {
     public class Database
     {
-
+        private string connectionstring = "Server =172.21.54.253;Database=25_132330053;User=25_132330053;Password=!nif.ogr53KA";
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionstring);
+        }
     }
 }
