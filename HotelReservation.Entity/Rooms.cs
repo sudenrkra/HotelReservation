1fc7c86ab410f22e1d0586a0eda97dbffa406574
Sude Nur Kara <sudenrkra@gmail.com>
2024-12-29 02:37:18 +0300

    init

--- HotelReservation.Entity/Rooms.cs
+++ HotelReservation.Entity/Rooms.cs
@@ -9,7 +9,7 @@ namespace HotelReservation.Entity
 {
     public class Rooms
     {
-        public int RoomId { get; set; }
+        private int RoomId { get; set; }
         public string RoomType { get; set; }
         public decimal Price { get; set; }
         public string State { get; set; } 
