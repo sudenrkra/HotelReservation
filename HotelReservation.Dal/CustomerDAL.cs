3c163596d5a5056e5e0228ca5da17c9ea4cb456b
Sude Nur Kara <sudenrkra@gmail.com>
2025-01-03 17:13:17 +0300

    init

---
+++ HotelReservation.Dal/CustomerDAL.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using HotelReservation.Entity;
+using System.Security.Cryptography;
+using System.Data;
+
+namespace HotelReservation.Dal
+{
+    public class CustomerDAL
+    {
+        private Database db = new Database();
+
+        //Create(Insert)
+        public void AddCustomer(Customers customer)
+        {
+            using (var connection = db.GetConnection())
+            {
+                connection.Open();
+                string query = "INSERT INTO Customers(CustomerName,CustomerSurname,Email,Phone) VALUES (@CustomerName,@CustomerSurname,@Email,@Phone)";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerName",customer.CustomerName);
+                    cmd.Parameters.AddWithValue("@CustomerSurname", customer.CustomerSurname);
+                    cmd.Parameters.AddWithValue("@Email", customer.Email);
+                    cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        //Read(Select)
+        public Customers GetCustomersById(int CustomerId)
+        {
+            Customers customer = null;
+            using (var connection = db.GetConnection())
+            {
+                connection.Open();
+                string query = "SELECT * FROM Customers WHERE CustomerId = @CustomerId";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", CustomerId);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            customer = new Customers();
+                            {
+                                customer.SetCustomerId( reader.GetInt32("CustomerId"));
+                                customer.CustomerName = reader.GetString("CustomerName");
+                                customer.CustomerSurname = reader.GetString("CustomerSurname");
+                                customer.Email = reader.GetString("Email");
+                                customer.Phone = reader.GetString("Phone");
+                            };
+                        }
+                    }
+                }
+            }
+            return customer;
+        }
+        //Update
+        public void UpdateCustomer(Customers customer)
+        {
+            using (var connection = db.GetConnection())
+            {
+                connection.Open();
+                string query = "UPDATE Customers SET CustomerName = @CustomerName, CustomerSurname=@CustomerSurname, Email=@Email, Phone=@Phone WHERE CustomerId=@CustomerId";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerName", customer.CustomerName);
+                    cmd.Parameters.AddWithValue("@CustomerSurname", customer.CustomerSurname);
+                    cmd.Parameters.AddWithValue("@Email", customer.Email);
+                    cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+                    cmd.Parameters.AddWithValue("@CustomerId", customer.GetCustomerId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        //Delete
+        public void  DeleteCustomers(int customerId)
+        {
+            using (var connection = db.GetConnection())
+            {
+                connection.Open();
+                string query = "DELETE FROM Customers WHERE CustomerId= @CustomerId";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- HotelReservation.Entity/Customers.cs
+++ HotelReservation.Entity/Customers.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace HotelReservation.Entity
 {
     public class Customers
@@ -13,5 +12,15 @@ namespace HotelReservation.Entity
         public string CustomerSurname { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        //sadece getter ve setter metotları üzerinden erişim sağlamama olanak tanır,veritabanı işlemleri sırasında güvenliği ve veri bütünlüğünü korumaya yardımcı olur.
+        public int GetCustomerId()
+        { 
+            return CustomerId; 
+        }
+        public void SetCustomerId(int value)
+        { 
+            CustomerId = value; 
+        }
     }
 }
