75c3899c900a69ef4b374faf3e360ebc670301b8
Sude Nur Kara <sudenrkra@gmail.com>
2025-01-04 00:41:22 +0300

    init

---
+++ HotelReservation.Dal/BillDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using HotelReservation.Entity;
+
+namespace HotelReservation.Dal
+{
+    public class BillDAL
+    {
+        private Database db = new Database();
+
+        //Create
+        public void AddBill(Bills bill)
+        {
+            using (var connection = db.GetConnection())
+            {
+                connection.Open();
+                string query = "INSERT INTO Bills(TotalPrice,BillDate,ReservationId,CustomerId) VALUES (@TotalPrice,@BillDate,@ReservationId,@CustomerId)";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@BillDate", bill.BillDate);
+                    cmd.Parameters.AddWithValue("@TotalPrice", bill.TotalPrice);
+                    cmd.Parameters.AddWithValue("@CustomerId", bill.CustomerId);
+                    cmd.Parameters.AddWithValue("@ReservationId", bill.ReservationId);
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
