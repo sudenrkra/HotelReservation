5c6e360ecbefe9ff48a09368d26e56dff79f7111
Sude Nur Kara <sudenrkra@gmail.com>
2025-01-08 21:07:32 +0300

    init

--- HotelReservation.Dal/Database.cs
+++ HotelReservation.Dal/Database.cs
@@ -4,8 +4,8 @@ namespace HotelReservation.Dal
 {
     public class Database
     {
-        //private string connectionString = "Server =172.21.54.253;Database=25_132330053;User=25_132330053;Password=!nif.ogr53KA";
-        private string connectionString = "Server =localhost;Port=3306;Database=hotelreservationdb;Uid=root;Pwd=;";
+        private string connectionString = "Server =172.21.54.253;Database=25_132330053;User=25_132330053;Password=!nif.ogr53KA";
+        //private string connectionString = "Server =localhost;Port=3306;Database=hotelreservationdb;Uid=root;Pwd=;";
         public MySqlConnection GetConnection()
         {
             return new MySqlConnection(connectionString);
